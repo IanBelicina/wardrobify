1a7b3c5828e06058b5fd14551b6ec688cf680069
Ian <vancebelicina@gmail.com>
2023-07-19 16:40:45 -0700

    added shoe model, created view function for GET, registered in url, included url in django project. Also tested API in insomnia and able to get empty shoe list.

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Shoe(models.Model):
+    manufacturer = models.CharField(max_length=200)
+    model_name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+    bin = models.CharField(max_length=200)
--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Shoe
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from common.json import ModelEncoder
+
+
+
+class ShoeEncoder(ModelEncoder):
+    model = Shoe
+    properties = [
+        "id",
+        "manufacturer",
+        "model_name",
+        "color",
+        "url",
+        "bin",
+    ]
 
 # Create your views here.
+
+@require_http_methods(["GET"])
+def api_shoes(request):
+
+    if request.method == "GET":
+        shoes = Shoe.objects.all()
+        return JsonResponse(
+            {"shoes":shoes},
+            encoder=ShoeEncoder
+        )
