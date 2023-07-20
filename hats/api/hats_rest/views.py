9c6c0bd1ffdd9df76d21f5221a376e8af6dc20d0
Tawain <tawainlanon@gmail.com>
2023-07-20 09:34:23 +0900

    addedhatsmodel

--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,29 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from .models import Hat
+import json
+from common.json import ModelEncoder
+
+
+class HatEncoder(ModelEncoder):
+    model = Hat
+    properties = [
+        "id",
+        "fabric",
+        "style_name",
+        "color",
+        "picture_url",
+        "location",
+    ]
+
 
 # Create your views here.
+def api_list_hats(request):
+    if request.method == "GET":
+        hats = Hat.objects.all()
+        return JsonResponse(
+            {"hats": hats},
+            encoder=HatEncoder,
+        )
+    else:
+        content = json.loads(request.body)
